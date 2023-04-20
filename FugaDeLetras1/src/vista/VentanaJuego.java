075a6bdca24b6529c50286a6add4e94a5e194f3e
Heisuzul <117621578+Heisuzul@users.noreply.github.com>
2023-04-20 11:44:18 -0500

    Update VentanaJuego.java

--- FugaDeLetras1/src/vista/VentanaJuego.java
+++ FugaDeLetras1/src/vista/VentanaJuego.java
@@ -14,6 +14,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -33,22 +36,46 @@ public class VentanaJuego extends JFrame{
     private JLabel jlJugador;
     private JLabel jlConsigna;
     private JPanel jpContenido;   
-    private JButton jOpcion1, jOpcion2, jOpcion3, jOpcion4, jOpcion5;
+    private JButton jOpcion1, jOpcion2, jOpcion3, jOpcion4, jOpcion5, jOpcion6;
     private JLabel jlNombreJugador;
+    private JLabel jlContadorAciertos,jlContadorFallos;
     private String nombreJugador;
-    private int posicionActual;
+    private int posicion;
     private int idTematica;
+    private char[] vocales = {'a', 'e', 'i', 'o', 'u'};
+    private String palabraCensurada;
+    private char vocalCensurada;
+    modelo.Tematica palabras = new Tematica();
+    Random random = new Random();
+    private String palabra;
+    private int contadorAciertos,contadorFallos = 0;
         
     public VentanaJuego(Jugador jugador, int idTematica){
        nombreJugador = jugador.getNombre();
-       iniciarComponentes();
-       posicionActual = 0;
        this.idTematica = idTematica;
+       IniciarJuego();
+       iniciarComponentes();
     }
+    
+    private void IniciarJuego () {
+        posicion = random.nextInt(9);
+        CensurarPalabra(); 
+        
+    }
+        private void CensurarPalabra(){
+        palabra = palabras.getPalabra(posicion,idTematica);
+        vocalCensurada = vocales[random.nextInt(vocales.length)];
+        if(palabra.indexOf(vocalCensurada) != -1 ){
+        palabraCensurada = palabra.replace(vocalCensurada, '_');}
+        else {
+            CensurarPalabra();
+        }
+        System.out.println(palabraCensurada);
+        }
     
         private void iniciarComponentes() {
             
-        modelo.Tematica palabras = new Tematica();
+        
         
         setTitle("Fuga de Letras");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -77,15 +104,26 @@ public class VentanaJuego extends JFrame{
         jlNombreJugador.setForeground( Color.WHITE);
         jlNombreJugador.setFont(new Font("arial", Font.BOLD, 15));
         jpContenido.add(jlNombreJugador);
-
         
-        jlConsigna = new JLabel("¿Qué letra falta en la siguiente palabra?:",SwingConstants.CENTER);
+        jlContadorAciertos = new JLabel("Aciertos: " + String.valueOf(contadorAciertos));
+        jlContadorAciertos.setBounds(2,235, 400,30);
+        jlContadorAciertos.setForeground( Color.WHITE);
+        jlContadorAciertos.setFont(new Font("arial", Font.BOLD, 15));
+        jpContenido.add(jlContadorAciertos); 
+        
+        jlContadorFallos = new JLabel("Fallos: " + String.valueOf(contadorFallos));
+        jlContadorFallos.setBounds(200,235, 400,30);
+        jlContadorFallos.setForeground( Color.WHITE);
+        jlContadorFallos.setFont(new Font("arial", Font.BOLD, 15));
+        jpContenido.add(jlContadorFallos);    
+        
+        jlConsigna = new JLabel("¿Qué vocal falta en la siguiente palabra?:",SwingConstants.CENTER);
         jlConsigna.setBounds(55,40, 400,30);
         jlConsigna.setForeground(Color.WHITE);
         jlConsigna.setFont(new Font("arial", Font.BOLD, 20));
         jpContenido.add(jlConsigna);
         
-        jlPalabra = new JLabel(palabras.getPalabra(posicionActual,idTematica),SwingConstants.CENTER);
+        jlPalabra = new JLabel(palabraCensurada,SwingConstants.CENTER);
         jlPalabra.setBounds(40,70, 440,50);
         jlPalabra.setForeground(Color.WHITE);
         jlPalabra.setFont(new Font("arial", Font.BOLD, 20));
@@ -113,6 +151,7 @@ public class VentanaJuego extends JFrame{
         jOpcion5.setBounds(374,140, 50,50);
         jpContenido.add(jOpcion5);
         
+        
         System.out.println(nombreJugador);
         
         ManejadorDeEventos manejadorEventos = new VentanaJuego.ManejadorDeEventos();
@@ -120,51 +159,102 @@ public class VentanaJuego extends JFrame{
         jOpcion1.addActionListener(manejadorEventos);
         jOpcion2.addActionListener(manejadorEventos);
         jOpcion3.addActionListener(manejadorEventos);
-         //no funciona del todo bien, ni idea de porqué
+        jOpcion4.addActionListener(manejadorEventos);
+        jOpcion5.addActionListener(manejadorEventos);
+        
+        addWindowListener(new WindowAdapter() {
+             @Override
+            public void windowClosing(WindowEvent evt) {
+                cerrarVentana();
+            }
+        });
     }
         
     class ManejadorDeEventos implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent evento){
             
-            modelo.Tematica palabras = new Tematica();
-            
-            if(evento.getSource() == jOpcion1){
-               posicionActual++;
-               if(posicionActual <= palabras.getLength(idTematica)){
-                    System.out.println(posicionActual);
-                    jlPalabra.setText(palabras.getPalabra(posicionActual,idTematica));
-                    //aquí iría el método que verificaría si es la opción correcta
-                    //Jugador jugador = new Jugador(super.savedNombre); 
-                    //VentanaJuego ventanaJuego = new VentanaJuego(jugador);
-               } else {
-                    JOptionPane.showMessageDialog(null,"El juego ha terminado", 
-                    "GAME OVER", JOptionPane.ERROR_MESSAGE);
-                    
-
-               }
-
+            if(evento.getSource() == jOpcion1 && vocalCensurada == 'a'){
+               jlPalabra.setText(palabra);
+               JOptionPane.showMessageDialog(null, "Es la vocal correcta");
+               IniciarJuego();
+               jlPalabra.setText(palabraCensurada);
+               contadorAciertos++;
+               jlContadorAciertos.setText("Aciertos: " + String.valueOf(contadorAciertos));
                
-
-            } else if (evento.getSource() == jOpcion2){
-               posicionActual++;
-               jlPalabra.setText(palabras.getPalabra(posicionActual,idTematica));
-               //aquí iría el método que verificaría si es la opción correcta
-               //Jugador jugador = new Jugador(super.savedNombre); 
-               //VentanaJuego ventanaJuego = new VentanaJuego(jugador); 
-            } else if (evento.getSource() == jOpcion3){
-               posicionActual++;
-               jlPalabra.setText(palabras.getPalabra(posicionActual,idTematica));
-               //aquí iría el método que verificaría si es la opción correcta
-               //Jugador jugador = new Jugador(super.savedNombre); 
-               //VentanaJuego ventanaJuego = new VentanaJuego(jugador); 
-            } else if (evento.getSource() == jOpcion4){
-               //aquí iría el método que verificaría si es la opción correcta
-               dispose();
-            } else if (evento.getSource() == jOpcion5){
-               //aquí iría el método que verificaría si es la opción correcta
-               dispose();
+            } else if (evento.getSource() == jOpcion2 && vocalCensurada == 'e' ){
+               jlPalabra.setText(palabra);
+               JOptionPane.showMessageDialog(null, "Es la vocal correcta"); 
+               IniciarJuego();
+               jlPalabra.setText(palabraCensurada);
+               contadorAciertos++;
+               jlContadorAciertos.setText("Aciertos: " + String.valueOf(contadorAciertos));
+               
+            } else if (evento.getSource() == jOpcion3 && vocalCensurada == 'i'){
+               jlPalabra.setText(palabra);
+               JOptionPane.showMessageDialog(null, "Es la vocal correcta"); 
+               IniciarJuego();
+               jlPalabra.setText(palabraCensurada);
+               contadorAciertos++;
+               jlContadorAciertos.setText("Aciertos: " + String.valueOf(contadorAciertos));
+               
+            } else if (evento.getSource() == jOpcion4 && vocalCensurada == 'o'){
+               jlPalabra.setText(palabra);
+                JOptionPane.showMessageDialog(null, "Es la vocal correcta");    
+                IniciarJuego();
+                jlPalabra.setText(palabraCensurada);
+                contadorAciertos++;
+                jlContadorAciertos.setText("Aciertos: " + String.valueOf(contadorAciertos));
+                
+            } else if (evento.getSource() == jOpcion5 && vocalCensurada == 'u'){
+               jlPalabra.setText(palabra);
+               JOptionPane.showMessageDialog(null, "Es la vocal correcta");    
+               IniciarJuego();
+               jlPalabra.setText(palabraCensurada);
+               contadorAciertos++;
+               jlContadorAciertos.setText("Aciertos: " + String.valueOf(contadorAciertos));
+               
+            } else {
+                JOptionPane.showMessageDialog(null, "Es la vocal incorrecta, intenta de nuevo");
+                contadorFallos++;
+                jlContadorFallos.setText("Fallos: " + String.valueOf(contadorFallos));
+                TerminarJuego();
             }  
+        }
+        
+    }
+    
+    
+    private void TerminarJuego () {
+    if (contadorFallos == 5){
+        int respuesta;
+        respuesta = JOptionPane.showConfirmDialog(
+                    null,"Se han acabado tus oportunidades, ¿deseas volver a empezar?", "GAME OVER",
+                    JOptionPane.YES_NO_OPTION, 
+                    JOptionPane.WARNING_MESSAGE);
+        if(respuesta == JOptionPane.YES_OPTION){
+            VentanaNombre ventana = new VentanaNombre();
+            dispose();
+        } else {
+             System.exit(0);
+        }
+    }
+        
+    }
+    
+    
+    
+
+    
+    private void cerrarVentana(){
+        int respuesta;
+
+        respuesta = JOptionPane.showConfirmDialog(
+                    null,"¿Realmente dese abandonar el juego?", "Advertencia",
+                    JOptionPane.YES_NO_OPTION, 
+                    JOptionPane.WARNING_MESSAGE);
+        if(respuesta == JOptionPane.YES_OPTION){
+            System.exit(0);
         }
     }
 } 
